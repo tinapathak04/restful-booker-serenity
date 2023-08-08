51a0b28233ad9bfc244c327550235ad38ddca43a
tinapathak04 <tinapathak040@gmail.com>
2023-08-08 21:27:43 +0100

    submitted

---
+++ src/main/java/com/restful/booker/constants/Path.java
@@ -0,0 +1,12 @@
+package com.restful.booker.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+    public static final String AUTHORIZATION = "/auth";
+    public static final String BOOKING = "/booking";
+
+
+
+}
